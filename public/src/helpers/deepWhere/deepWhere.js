b0431d5b3e874f19f9dbdf9b8bb12db831580621
Oleksii An <Oleksii_An@epam.com>
2015-11-03 16:04:47 +0200

    ARCH-0011: added deepWhere helper.

---
+++ public/src/helpers/deepWhere/deepWhere.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+
+let deepWhere = (collection, predicate) => {
+    let ret = [];
+
+    function rec(collection) {
+        ret.push(..._.where(collection, predicate));
+
+        if (_.isObject(collection)) {
+            _.forEach(collection, value => {
+                rec(value);
+            });
+        }
+    }
+
+    rec(collection);
+
+    return ret;
+};
+
+_.mixin({
+    deepWhere: deepWhere
+});
